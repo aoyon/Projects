bedabfaa13a1901ee6004db7c715a0536a5b74c6
aoyon <aoyon7@gmail.com>
2016-09-21 18:20:35 +0600

    Add files via upload

---
+++ SPI_LCD_Demo/SPI_LCD_Demo.ino
@@ -0,0 +1,16 @@
+#include <SPI.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(10);  //(or the latch pin of the 74HC595)
+
+void setup() { 
+  lcd.begin(16, 2);
+  lcd.print("SPI LCD Demo");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print("By SidewindeR");
+  lcd.setCursor(15, 1);
+  lcd.print(millis()/1000);
+}

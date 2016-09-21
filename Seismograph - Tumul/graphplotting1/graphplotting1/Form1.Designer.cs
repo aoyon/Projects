cdd5fadbce3dd510d275682f7c34a59fce574f93
aoyon <aoyon7@gmail.com>
2016-09-21 18:14:57 +0600

    Add files via upload

---
+++ Seismograph - Tumul/graphplotting1/graphplotting1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace graphplotting1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.sensor1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSerialPort = new System.Windows.Forms.TextBox();
+            this.buttonSerialPortConn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonClearGraph = new System.Windows.Forms.Button();
+            this.buttonSerialPortDC = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sensor1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sensor1
+            // 
+            this.sensor1.BorderlineColor = System.Drawing.SystemColors.ButtonFace;
+            chartArea2.Name = "ChartArea1";
+            this.sensor1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.sensor1.Legends.Add(legend2);
+            this.sensor1.Location = new System.Drawing.Point(-16, 78);
+            this.sensor1.Margin = new System.Windows.Forms.Padding(4);
+            this.sensor1.Name = "sensor1";
+            this.sensor1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.sensor1.Series.Add(series2);
+            this.sensor1.Size = new System.Drawing.Size(917, 785);
+            this.sensor1.TabIndex = 0;
+            this.sensor1.Text = "Sensor 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serial Port :";
+            // 
+            // textBoxSerialPort
+            // 
+            this.textBoxSerialPort.Location = new System.Drawing.Point(120, 24);
+            this.textBoxSerialPort.Name = "textBoxSerialPort";
+            this.textBoxSerialPort.Size = new System.Drawing.Size(23, 22);
+            this.textBoxSerialPort.TabIndex = 2;
+            this.textBoxSerialPort.Text = "9";
+            // 
+            // buttonSerialPortConn
+            // 
+            this.buttonSerialPortConn.Location = new System.Drawing.Point(149, 23);
+            this.buttonSerialPortConn.Name = "buttonSerialPortConn";
+            this.buttonSerialPortConn.Size = new System.Drawing.Size(92, 23);
+            this.buttonSerialPortConn.TabIndex = 3;
+            this.buttonSerialPortConn.Text = "Connect";
+            this.buttonSerialPortConn.UseVisualStyleBackColor = true;
+            this.buttonSerialPortConn.Click += new System.EventHandler(this.buttonSerialPortConn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonClearGraph);
+            this.groupBox1.Controls.Add(this.buttonSerialPortDC);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxSerialPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonSerialPortConn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(702, 59);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Initializing";
+            // 
+            // buttonClearGraph
+            // 
+            this.buttonClearGraph.Location = new System.Drawing.Point(474, 22);
+            this.buttonClearGraph.Name = "buttonClearGraph";
+            this.buttonClearGraph.Size = new System.Drawing.Size(67, 23);
+            this.buttonClearGraph.TabIndex = 7;
+            this.buttonClearGraph.Text = "RePlot";
+            this.buttonClearGraph.UseVisualStyleBackColor = true;
+            this.buttonClearGraph.Click += new System.EventHandler(this.buttonClearGraph_Click);
+            // 
+            // buttonSerialPortDC
+            // 
+            this.buttonSerialPortDC.Location = new System.Drawing.Point(247, 23);
+            this.buttonSerialPortDC.Name = "buttonSerialPortDC";
+            this.buttonSerialPortDC.Size = new System.Drawing.Size(92, 23);
+            this.buttonSerialPortDC.TabIndex = 6;
+            this.buttonSerialPortDC.Text = "Disconnect";
+            this.buttonSerialPortDC.UseVisualStyleBackColor = true;
+            this.buttonSerialPortDC.Click += new System.EventHandler(this.buttonSerialPortDC_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sensor Value : 0000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "COM";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(this.serialPort1_ErrorReceived);
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.progressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 454);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(734, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(122, 17);
+            this.toolStripStatusLabel1.Text = "Connect to COM Port";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(1, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 160);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "S\r\ny\r\ns\r\nm\r\no\r\nm\r\ne\r\nt\r\ne\r\nr";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(342, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 476);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.sensor1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sysmometer - CUET";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.sensor1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart sensor1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSerialPort;
+        private System.Windows.Forms.Button buttonSerialPortConn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSerialPortDC;
+        private System.Windows.Forms.Button buttonClearGraph;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+    }
+}
+
---
+++ Seismograph - Tumul/graphplotting1/graphplotting1/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace graphplotting1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadGraph();
+        }
+
+        public void loadGraph()
+        {
+            sensor1.ChartAreas.Add("first");
+            sensor1.Series.Add("sensor1YValue");
+            sensor1.Series["sensor1YValue"].ChartType = SeriesChartType.Line;
+
+            sensor1.ChartAreas["first"].AxisY.Maximum = 700;
+            sensor1.ChartAreas["first"].AxisY.Minimum = 300;
+            sensor1.ChartAreas["first"].AxisY.Interval=1;
+        }
+
+        private void buttonSerialPortConn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.PortName = string.Format("COM{0}", textBoxSerialPort.Text);
+                    serialPort1.Open();
+                    serialPort1.ReadTimeout = 500;
+                    if (serialPort1.IsOpen)
+                    {
+                        progressBar1.Value = 100;
+                        toolStripStatusLabel1.Text = string.Format("COM{0} has Opened : Ready to Use", textBoxSerialPort.Text);
+                    }
+                    if (!serialPort1.IsOpen)
+                    {
+                        progressBar1.Value = 0;
+                        toolStripStatusLabel1.Text = "Failed to Initialize Ports";
+                    }
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show(String.Format("COM{0} is not available", textBoxSerialPort.Text));
+            }
+        }
+
+        private void buttonSerialPortDC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    if (serialPort1.IsOpen)
+                    {
+                        progressBar1.Value = 0;
+                        toolStripStatusLabel1.Text = string.Format("COM{0} failed to close", textBoxSerialPort.Text);
+                    }
+                    if (!serialPort1.IsOpen)
+                    {
+                        progressBar1.Value = 100;
+                        toolStripStatusLabel1.Text = string.Format("COM{0} has close", textBoxSerialPort.Text);
+                        sensor1.Series["sensor1YValue"].Points.Clear();
+                    }
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show(String.Format("COM{0} failed to close", textBoxSerialPort.Text));
+            }
+        }
+
+        private void serialPort1_ErrorReceived(object sender, System.IO.Ports.SerialErrorReceivedEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                if (serialPort1.IsOpen)
+                {
+                    progressBar1.Value = 0;
+                    toolStripStatusLabel1.Text = string.Format("COM{0} failed to close", textBoxSerialPort.Text);
+                }
+                if (!serialPort1.IsOpen)
+                {
+                    progressBar1.Value = 100;
+                    toolStripStatusLabel1.Text = string.Format("COM{0} has close", textBoxSerialPort.Text);
+                    sensor1.Series["sensor1YValue"].Points.Clear();
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show(String.Format("COM{0} failed to close", textBoxSerialPort.Text));
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string sData;
+            Int32 sDataval;
+            try
+            {
+                sData = serialPort1.ReadLine();
+                //sDataval = Convert.ToInt32(sData);
+                UpdateAnalog(sData);
+            }
+
+            catch (TimeoutException)
+            {
+                MessageBox.Show("Timed Out!");
+            }
+        } 
+        
+        delegate void UpdateAnalogDelegate(string analogVal);
+
+        void UpdateAnalog(string analogVal)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new UpdateAnalogDelegate(UpdateAnalog), analogVal);
+                return;
+            }
+            
+            int progVal;
+
+            try
+            {
+                if (int.TryParse(analogVal, out progVal))
+                {
+                    label3.Text = string.Format("Sensor Value : {0}", analogVal);
+                    sensor1.Series["sensor1YValue"].Points.AddY(analogVal);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("WOW! Somwthing went wrong");
+            }
+        }
+
+
+        private void buttonClearGraph_Click(object sender, EventArgs e)
+        {
+            sensor1.Series["sensor1YValue"].Points.Clear();
+            toolStripStatusLabel1.Text = "Graphics Area Cleared";
+        }
+    }
+}

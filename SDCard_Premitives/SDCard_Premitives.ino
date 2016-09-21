c44c535275a287a3c6bcf535c6706e202d6609b7
aoyon <aoyon7@gmail.com>
2016-09-21 18:14:20 +0600

    Add files via upload

---
+++ SDCard_Premitives/SDCard_Premitives.ino
@@ -0,0 +1,40 @@
+#include <SD.h>
+
+int CSPin = 2;
+int n = 0;
+void setup()
+{
+  pinMode(CSPin ,OUTPUT);
+  
+  Serial.begin(9600);
+  Serial.println("Initializing SD Card");
+  
+  if(!SD.begin(CSPin))
+  {
+    Serial.println("SD Card Failed :(");
+    return;
+  }
+  Serial.println("SD Card Ready :)");
+}
+
+void loop()
+{
+  File myFile = SD.open("myFile.txt",FILE_WRITE);
+  n++;
+  if(myFile)
+  {
+    myFile.print("SidewindeR's SD Card Project ");
+    myFile.println(n);
+    myFile.close();
+    Serial.print("SidewindeR's SD Card Project ");
+    Serial.println(n);
+  }
+  else
+  {
+    Serial.println("Faild to open the file");
+  }
+  
+  delay(5000);
+}
+  
+  
---
+++ SDCard_Temperature_Logging/SDCard_Temperature_Logging.ino
@@ -0,0 +1,67 @@
+#include <SD.h>
+
+int CSPin = 10;
+float average = 0;
+float tempC;
+int tempPin = 0;
+int n = 0;
+void setup()
+{
+  pinMode(CSPin ,OUTPUT);
+  analogReference(INTERNAL);
+  Serial.begin(9600);
+  Serial.println("Initializing SD Card");
+  
+  if(!SD.begin(CSPin))
+  {
+    Serial.println("SD Card Failed :(");
+    return;
+  }
+  Serial.println("SD Card Ready :)");
+  
+  File logFile = SD.open("TempLOG.csv",FILE_WRITE);
+  
+  if(logFile)
+  {
+    logFile.println("ID, Temp C");
+    logFile.close();
+    Serial.println("ID, Temp C");
+  }
+  else
+  {
+    Serial.println("Faild to open TempLOG.csv");
+    return;
+  }
+}
+
+void loop()
+{
+  average = 0;
+  for(int i = 0;i < 100;i++)
+  {
+    average = average + analogRead(tempPin);
+    delay(10);
+  }
+  tempC = average / 100;
+  //tempC = (5.0 * tempC * 100.0)/1024.0;
+  tempC = tempC/9.310;
+  File logFile = SD.open("TempLOG.csv",FILE_WRITE);
+  
+  if(logFile)
+  {
+    logFile.print(n);
+    logFile.print(", ");
+    logFile.println(tempC);
+    logFile.close();
+    Serial.print(n);
+    Serial.print(", ");
+    Serial.println(tempC);
+  }
+  else
+  {
+    Serial.println("Faild to open the file");
+  }
+  n++;
+}
+  
+  

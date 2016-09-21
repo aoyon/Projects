9eb175896700dbba6b0e7d38758776b64974ef02
aoyon <aoyon7@gmail.com>
2016-09-21 18:05:00 +0600

    Add files via upload

---
+++ Bluetooth_HC05/Bluetooth_HC05.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h>
+
+#define rxPin 2
+#define txPin 3
+
+SoftwareSerial mySerial(rxPin, txPin); // RX, TX
+String readString;
+int ledPin = 13;
+
+void setup()
+{
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(ledPin, OUTPUT); 
+}
+
+void loop()
+{
+  while (mySerial.available()) {
+    delay(3);  
+    char c = mySerial.read();
+    readString += c; 
+  }
+  if (readString.length() >0) {
+    Serial.println(readString);
+    if (readString == "on")     
+    {
+      digitalWrite(ledPin, HIGH);
+    }
+    if (readString == "off")
+    {
+      digitalWrite(ledPin, LOW);
+    }
+    readString="";
+  } 
+}
+

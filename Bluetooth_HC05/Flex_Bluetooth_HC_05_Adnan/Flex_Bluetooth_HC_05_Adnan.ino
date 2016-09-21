da15c168a257c4cb13961c28346b90210be3c3e3
aoyon <aoyon7@gmail.com>
2016-09-21 18:05:54 +0600

    Add files via upload

---
+++ Bluetooth_HC05/Flex_Bluetooth_HC_05_Adnan/Flex_Bluetooth_HC_05_Adnan.ino
@@ -0,0 +1,24 @@
+/* Serial Loop */
+int flexSensorPin1 = A0; //analog pin 0
+int flexSensorReading1 = 0;
+#include <SoftwareSerial.h>
+
+#define rxPin 10
+#define txPin 11
+
+SoftwareSerial BTSerial(rxPin, txPin);
+
+void setup() {
+  Serial.begin(9600);
+  delay(3000);
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  flexSensorReading1 = analogRead(flexSensorPin1);
+  Serial.println(flexSensorReading1);
+  BTSerial.println(flexSensorReading1);
+  delay(300);
+}
+
+
